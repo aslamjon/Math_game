73a61ad3e87dc4fa7289ad9ea893c2c67f81d220
aslamjon <aslamjon.ibragimov@gmail.com>
2021-04-20 14:32:13 +0500

    fixed

--- js/main.js
+++ js/main.js
@@ -6,46 +6,46 @@ let checked_switch = `
 
 let selected = 0;
 document.querySelectorAll('.maxrandom').forEach((value, index) => {
-        value.onclick = function() {
-            if (index == 0){
-                selected = index;
-            }
-            else{
-                selected = index;
-            }
+    value.onclick = function () {
+        if (index == 0) {
+            selected = index;
         }
+        else {
+            selected = index;
+        }
+    }
 })
 
 let ifChecked = document.getElementById('ifChecked');
 let checkbox = document.getElementById('selectrandom');
-checkbox.onclick = function() {
-    if(checkbox.checked) {
+checkbox.onclick = function () {
+    if (checkbox.checked) {
         document.querySelectorAll('.maxrandom').forEach((value, index) => {
             value.style.display = 'none';
             value.checked = false;
         })
-        document.querySelectorAll('.span').forEach((value,index) => {
+        document.querySelectorAll('.span').forEach((value, index) => {
             value.style.display = 'none';
         })
         ifChecked.style.display = 'block';
         selected = 5;
     }
-    else{
+    else {
         document.querySelectorAll('.maxrandom').forEach((value, index) => {
             value.style.display = 'inline-block';
             value.checked = false;
         })
-        document.querySelectorAll('.span').forEach((value,index) => {
+        document.querySelectorAll('.span').forEach((value, index) => {
             value.style.display = 'inline-block';
         })
         ifChecked.style.display = 'none';
     }
 }
-document.getElementById('btn').addEventListener('click', function(){
+document.getElementById('btn').addEventListener('click', function () {
     main(selected);
 })
 function main(index) {
-    let amal = document.querySelector('select').value; 
+    let amal = document.querySelector('select').value;
     let misol = document.getElementById('misol');
     let javobInput = document.getElementById('javobInput');
     javobInput.innerHTML = `<input type="number" class="form-control" id="javob"> 
@@ -66,27 +66,29 @@ function main(index) {
     let errorNum = error.innerHTML;
     errorNum *= 1;
 
-    
-    function thisTrue(){
+    success.style.display = 'none';
+    danger.style.display = 'none';
+
+    function thisTrue() {
         success.style.display = 'none';
         danger.style.display = 'none';
-        success.style.display='block';
+        success.style.display = 'block';
         levelNum += 1;
         level.innerHTML = levelNum;
-        send.setAttribute('disabled','true');
+        send.setAttribute('disabled', 'true');
         document.getElementById('btn').removeAttribute('disabled');
     }
-    function thisFalse(){
+    function thisFalse() {
         if ((errorNum - 9) == levelNum) {
             let form = document.getElementById('form').children;
-            for (let i=0; form.length > i; i++){
+            for (let i = 0; form.length > i; i++) {
                 form[i].style.display = 'none';
             }
             document.getElementById('game-over').style.display = 'block';
             document.getElementById('game-over').style.height = '100%';
             document.getElementById('game-over').style.opacity = 1;
             document.getElementById('game-over').style.transform = 'translateY(0px)';
-            document.getElementById('game-over').onclick = function(){
+            document.getElementById('game-over').onclick = function () {
                 window.location.reload();
             }
         } else {
@@ -95,133 +97,133 @@ function main(index) {
             danger.style.display = 'block';
             errorNum += 1;
             error.innerHTML = errorNum;
-            document.getElementById('btn').setAttribute('disabled','true');
+            document.getElementById('btn').setAttribute('disabled', 'true');
         }
     }
 
-    if (selected == 0){
+    if (selected == 0) {
         let result1 = Math.floor(Math.random() * 11);
         let result2 = Math.floor(Math.random() * 11);
         if (amal == '+') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1+result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 + result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '-') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1-result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 - result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '*') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1*result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 * result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '/') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1/result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 / result2) ? thisTrue() : thisFalse();
             }
         }
     }
-    else if (selected == 1){
+    else if (selected == 1) {
         console.log(Math.floor(Math.random() * 21));
         let result1 = Math.floor(Math.random() * 21);
         let result2 = Math.floor(Math.random() * 21);
         if (amal == '+') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1+result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 + result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '-') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1-result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 - result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '*') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1*result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 * result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '/') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1/result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 / result2) ? thisTrue() : thisFalse();
             }
         }
     }
-    else if (selected == 2){
+    else if (selected == 2) {
         let result1 = Math.floor(Math.random() * 31);
         let result2 = Math.floor(Math.random() * 31);
         if (amal == '+') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1+result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 + result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '-') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1-result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 - result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '*') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1*result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 * result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '/') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1/result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 / result2) ? thisTrue() : thisFalse();
             }
         }
     }
-    else if (selected == 3){
+    else if (selected == 3) {
         let result1 = Math.floor(Math.random() * 41);
         let result2 = Math.floor(Math.random() * 41);
         if (amal == '+') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1+result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 + result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '-') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1-result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 - result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '*') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1*result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 * result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '/') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1/result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 / result2) ? thisTrue() : thisFalse();
             }
         }
     }
-    else if (selected == 4){
+    else if (selected == 4) {
         let result1 = Math.floor(Math.random() * 51);
         let result2 = Math.floor(Math.random() * 51);
         if (amal == '+') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1+result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 + result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '-') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1-result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 - result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '*') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1*result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 * result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '/') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1/result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 / result2) ? thisTrue() : thisFalse();
             }
         }
     }
@@ -230,24 +232,24 @@ function main(index) {
         let result1 = Math.floor(Math.random() * Number(ifChecked.value));
         let result2 = Math.floor(Math.random() * Number(ifChecked.value));
         if (amal == '+') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1+result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 + result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '-') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1-result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 - result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '*') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1*result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 * result2) ? thisTrue() : thisFalse();
             }
         } else if (amal == '/') {
-            misol.innerHTML = '' + result1 + ' '+ amal + ' ' + result2;
-            document.getElementById('send').onclick = function(){
-                (javob.value == result1/result2) ? thisTrue() : thisFalse();
+            misol.innerHTML = '' + result1 + ' ' + amal + ' ' + result2;
+            document.getElementById('send').onclick = function () {
+                (javob.value == result1 / result2) ? thisTrue() : thisFalse();
             }
         }
     }
